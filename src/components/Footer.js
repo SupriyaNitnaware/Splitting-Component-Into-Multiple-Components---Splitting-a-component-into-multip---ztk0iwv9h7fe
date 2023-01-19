8d0d8de895823365ff1772138a2d11249f0800f8
Supriya_Nitnaware <atg.supriyanitnaware@gmail.com>
2023-01-19 23:05:54 +0530

    props

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,14 @@
 
+import React from 'react'
+
+const Footer = (props) => {
+  return (
+    <div id='footer'>
+        <footer id={props.id}>
+            <div>{props.content}</div>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,13 @@
+import React from 'react'
 
+const Header = (props) => {
+  return (
+    <div id='header'>
+        <header id={props.id}>
+            <div>{props.content}</div>
+        </header>
+    </div>
+  )
+}
+
+export default Header
