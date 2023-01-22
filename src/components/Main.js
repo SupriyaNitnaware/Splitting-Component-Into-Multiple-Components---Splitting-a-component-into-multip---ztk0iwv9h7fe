b86d3b4e2db8a9ae22fb8db0b56c3a72b5c66f36
Supriya Nitnaware <80514079+SupriyaNitnaware@users.noreply.github.com>
2023-01-22 18:13:53 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Main = (props) => {
   return (
     <div id='main'>
-        <main id={props.id}>
-            <div>{props.content}</div>
-        </main>
+    <main id={props.id}>
+    <div>{props.content}</div>
+    </main>
     </div>
   )
 }
 
-export default Main
+export default Main;
